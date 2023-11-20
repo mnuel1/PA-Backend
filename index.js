28aafb5b142fc8c958b4c18e84ae3c30be0b0acd
Manuel <mrnmnuel21@gmail.com>
2023-11-20 16:42:12 +0800

    added connection

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+const morgan = require('morgan');
+const cors = require('cors');
+
+const connection = require('./src/configs/connection');
+require('dotenv').config();
+
+const port = process.env.PORT || 4000; // Use process.env to access environment variables
+
+
+// Check database connection
+connection
+  .query('SELECT NOW() as now')
+  .then(result => {
+    console.log('Connected to PostgreSQL database at:', result.rows[0].now);
+  })
+  .catch(error => {
+    console.error('Error connecting to PostgreSQL database', error);
+  });
+
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
