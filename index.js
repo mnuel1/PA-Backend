209bb40c64e108d76a6c75b23f902328bc536bf0
finnsatoshi03 <finnsatoshi03@gmail.com>
2023-12-05 17:38:33 +0800

    Added delete function and fix bugs

--- index.js
+++ index.js
@@ -1,94 +1,94 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const cors = require('cors');
+const morgan = require("morgan");
+const cors = require("cors");
 
 // CONTROLLERS
-const userController = require('./src/controllers/UserController')
-const adminController = require('./src/controllers/AdminController')
-const verificationController = require('./src/controllers/VerificationController')
-const notificationController = require('./src/controllers/NotificationController')
-const userEventController = require('./src/controllers/UserEventController')  
-const adminEventController = require('./src/controllers/AdminEventController') 
+const userController = require("./src/controllers/UserController");
+const adminController = require("./src/controllers/AdminController");
+const verificationController = require("./src/controllers/VerificationController");
+const notificationController = require("./src/controllers/NotificationController");
+const userEventController = require("./src/controllers/UserEventController");
+const adminEventController = require("./src/controllers/AdminEventController");
 
-const connection = require('./src/configs/connection');
-require('dotenv').config();
+const connection = require("./src/configs/connection");
+require("dotenv").config();
 
-const port = process.env.PORT || 4000; 
+const port = process.env.PORT || 4000;
 
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const multer = require('multer');
+const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
+    cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
 const upload = multer({ storage: storage });
 
-app.post('/upload', upload.single('file'), (req, res) => {
-  res.json({ message: 'File uploaded successfully' });
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.json({ message: "File uploaded successfully" });
 });
 
-app.post('/adminlogin',adminController.AdminLogin)
-app.patch('/editAdmin',adminController.editAdmin);
+app.post("/adminlogin", adminController.AdminLogin);
+app.patch("/editAdmin", adminController.editAdmin);
 
-app.post('/userlogin',userController.Login)
-app.post('/register', userController.Register);
-app.get('/retrieveVUsers',userController.retrieveVerifiedUsers);
-app.get('/retrieveNVUsers',userController.retrieveNotVerifiedUsers);
-app.patch('/editUser',userController.editUser);
+app.post("/userlogin", userController.Login);
+app.post("/register", userController.Register);
+app.get("/retrieveVUsers", userController.retrieveVerifiedUsers);
+app.get("/retrieveNVUsers", userController.retrieveNotVerifiedUsers);
+app.patch("/editUser", userController.editUser);
 
-app.patch('/verify',verificationController.Verified);
+app.patch("/verify", verificationController.Verified);
 
-app.post('/sendNotification',notificationController.sendNotification);
-app.patch('/setNotifRead',notificationController.modifyNotification);
-app.get('/getNotifications',notificationController.retrieveNotification);
+app.post("/sendNotification", notificationController.sendNotification);
+app.patch("/setNotifRead", notificationController.modifyNotification);
+app.get("/getNotifications", notificationController.retrieveNotification);
 
-app.get('/getAdminNotification',notificationController.retrieveAdminNotification)
+app.get(
+  "/getAdminNotification",
+  notificationController.retrieveAdminNotification
+);
 
 // user
-app.patch('/stateNotification',notificationController.stateNotification);
+app.patch("/stateNotification", notificationController.stateNotification);
 
+app.get("/userViewEvents", userEventController.userViewEvents);
+app.get("/getEvents", userEventController.viewEvents);
+app.get("/getParticipant", userEventController.viewParticipants);
+app.patch("/starredEvent", userEventController.starredEvent);
 
-app.get('/userViewEvents',userEventController.userViewEvents);
-app.get('/getEvents',userEventController.viewEvents);
-app.get('/getParticipant',userEventController.viewParticipants);
-app.patch('/starredEvent',userEventController.starredEvent);
+app.post("/createAttendance", userEventController.createAttendance);
 
-app.post('/createAttendance',userEventController.createAttendance)
+app.post("/createEvent", adminEventController.createEvent);
+app.patch("/editEvent", adminEventController.editEvent);
+app.delete("/deleteEvent", adminEventController.deleteEvent);
 
+app.post("/createReport", adminEventController.createReport);
+app.delete("/deleteReport", adminEventController.deleteReport);
+app.get("/getReports", adminEventController.getReport);
 
-
-app.post('/createEvent',adminEventController.createEvent);
-app.patch('/editEvent',adminEventController.editEvent);
-app.delete('/deleteEvent',adminEventController.deleteEvent);
-
-app.post('/createReport',adminEventController.createReport);
-app.get('/getReports', adminEventController.getReport)
-
-app.post('/addParticipant',adminEventController.addParticipant);
-app.put('/updateParticipants',adminEventController.updateParticipant)
-app.delete('/removeParticipant',adminEventController.removeParticipant);
+app.post("/addParticipant", adminEventController.addParticipant);
+app.put("/updateParticipants", adminEventController.updateParticipant);
+app.delete("/removeParticipant", adminEventController.removeParticipant);
 
 // attendees
-app.patch('/updateAttendee',adminEventController.presentAbsentAttendee)
-app.get('/getAttendees',adminEventController.getAttendees)
+app.patch("/updateAttendee", adminEventController.presentAbsentAttendee);
+app.get("/getAttendees", adminEventController.getAttendees);
 
+app.get("/getPresents", adminEventController.getPresents);
+app.get("/getAbsents", adminEventController.getAbsents);
 
-app.get('/getPresents',adminEventController.getPresents)
-app.get('/getAbsents',adminEventController.getAbsents)
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
 app.listen(port, () => {
--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -1,112 +1,166 @@
-const expressAsyncHandler = require('express-async-handler');
-const connection = require('../configs/connection');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const multer = require('multer');
-const { validateUserData } = require('../middlewares/ValidateUserData');
-require('dotenv').config();
+const expressAsyncHandler = require("express-async-handler");
+const connection = require("../configs/connection");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const { validateUserData } = require("../middlewares/ValidateUserData");
+require("dotenv").config();
 
 // Set up multer storage
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/'); // Specify the directory where you want to store the uploaded files
-    },
-    filename: function (req, file, cb) {
-      cb(null, Date.now() + '-' + file.originalname); // Use a unique filename to avoid overwriting files
-    },
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); // Specify the directory where you want to store the uploaded files
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname); // Use a unique filename to avoid overwriting files
+  },
 });
 
 const upload = multer({ storage: storage });
 
 const Register = expressAsyncHandler(async (req, res) => {
+  const {
+    username,
+    email,
+    contact,
+    password,
+    fullname,
+    employment_id,
+    office,
+  } = req.body;
 
-    const { username, email, contact, password, fullname, employment_id, office } = req.body;
-    
-    validateUserData(req, res, async () => {
-        try {
-            
-
-            connection.query(
-                `INSERT INTO pa_admin (username, email, password, fullname,
+  validateUserData(req, res, async () => {
+    try {
+      connection.query(
+        `INSERT INTO pa_admin (username, email, password, fullname,
                 employment_id, office, contact, image, verify) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-                [username, email, password, fullname, employment_id, office, contact],
-                (err, result) => {
-                    if (err) {
-                        console.error('Error inserting user:', err);
-                        res.status(500).json({ title: 'Internal Error', message: 'Registration failed. Please try again later.' });
-                    } else {
-                        res.status(200).json({ title: 'Success', message: 'Registered Successfully' });
-                    }
-                }
-            );
-        } catch (error) {
-            console.error('Error hashing password:', error);
-            res.status(500).json({ title: 'Internal Error', message: 'Registration failed. Please try again later.' });
+        [username, email, password, fullname, employment_id, office, contact],
+        (err, result) => {
+          if (err) {
+            console.error("Error inserting user:", err);
+            res
+              .status(500)
+              .json({
+                title: "Internal Error",
+                message: "Registration failed. Please try again later.",
+              });
+          } else {
+            res
+              .status(200)
+              .json({ title: "Success", message: "Registered Successfully" });
+          }
         }
-    });
-})
+      );
+    } catch (error) {
+      console.error("Error hashing password:", error);
+      res
+        .status(500)
+        .json({
+          title: "Internal Error",
+          message: "Registration failed. Please try again later.",
+        });
+    }
+  });
+});
 
 const AdminLogin = expressAsyncHandler(async (req, res) => {
-    const { username, password, checked } = req.body;
-    connection.query('SELECT * FROM pa_admin WHERE username = $1 AND password = $2', [username, password], (err, result) => {        
-        if(err){
-            res.status(500).json({title: 'Internal Error', message: err.message});
-        }
-        if(result.rows.length > 0){
-            const { id, fullname, contact, email, image } = result.rows[0]; //eto
-            const token = jwt.sign(
-                {username: username, user_id: id, fullname: fullname, role: 'admin', contact, email}, //eto
-                process.env.JWT_TOKEN,
-                {expiresIn: checked ? '1d': '7d'}
-            )
-            const img = Buffer.from(image, 'binary').toString('base64');
-            res.status(200).json({title: "Success", message: "Login Successful", token: token, username: username, user_id: id, 
-            fullname: fullname, role: 'admin', contact, email, img});
-        
-        }else{
-            res.status(401).json({title: "Login Error" ,message: "Credentials Incorrect"})
-        }
-    })
-})
-
+  const { username, password, checked } = req.body;
+  connection.query(
+    "SELECT * FROM pa_admin WHERE username = $1 AND password = $2",
+    [username, password],
+    (err, result) => {
+      if (err) {
+        res.status(500).json({ title: "Internal Error", message: err.message });
+      }
+      if (result.rows.length > 0) {
+        const { id, fullname, contact, email, image } = result.rows[0];
+        const token = jwt.sign(
+          {
+            username: username,
+            user_id: id,
+            fullname: fullname,
+            role: "admin",
+            contact,
+            email,
+          },
+          process.env.JWT_TOKEN,
+          { expiresIn: checked ? "1d" : "7d" }
+        );
+        const img = image
+          ? Buffer.from(image, "binary").toString("base64")
+          : null;
+        res
+          .status(200)
+          .json({
+            title: "Success",
+            message: "Login Successful",
+            token: token,
+            username: username,
+            user_id: id,
+            fullname: fullname,
+            role: "admin",
+            contact,
+            email,
+            img,
+          });
+      } else {
+        res
+          .status(401)
+          .json({ title: "Login Error", message: "Credentials Incorrect" });
+      }
+    }
+  );
+});
 
 const editAdmin = expressAsyncHandler(async (req, res) => {
+  // Use 'upload.single' middleware to handle the file upload
+  upload.single("image")(req, res, async (err) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ title: "Internal Error", message: "Image upload failed." });
+    } else {
+      console.log(req.body);
+    }
 
-    // Use 'upload.single' middleware to handle the file upload
-    upload.single('image')(req, res, async (err) => {
-        if (err) {
-            return res.status(500).json({ title: 'Internal Error', message: 'Image upload failed.' });
-        } else {
-            console.log(req.body);
-        }
+    const { user_id, username, email, contact, image } = req.body;
+    const imagePath = image.uri ? image.uri : null;
 
-        const { user_id, username, email, contact, image} = req.body;
-        const imagePath = image.uri ? image.uri : null; 
+    console.log(imagePath);
 
-        console.log(imagePath);
-        
-        validateUserData(req, res, async () => {
-            try {
-                connection.query(
-                `UPDATE pa_admin 
+    validateUserData(req, res, async () => {
+      try {
+        connection.query(
+          `UPDATE pa_admin 
                 SET username = $1, email = $2, contact = $3, image = $4 
                 WHERE id = $5`,
-                [username, email, contact, imagePath, user_id],
-                (err, result) => {
-                    if (err) {
-                    console.error('Error updating user:', err);
-                    res.status(500).json({ title: 'Something went wrong.', message: 'Update failed. Please try again later.' });
-                    } else {
-                    res.status(200).json({ title: 'Success', message: 'Updated Successfully' });
-                    }
-                }
-                );
-            } catch (error) {
-                res.status(500).json({ title: 'Something went wrong.', message: 'Update failed. Please try again later.' });
+          [username, email, contact, imagePath, user_id],
+          (err, result) => {
+            if (err) {
+              console.error("Error updating user:", err);
+              res
+                .status(500)
+                .json({
+                  title: "Something went wrong.",
+                  message: "Update failed. Please try again later.",
+                });
+            } else {
+              res
+                .status(200)
+                .json({ title: "Success", message: "Updated Successfully" });
             }
-        });
+          }
+        );
+      } catch (error) {
+        res
+          .status(500)
+          .json({
+            title: "Something went wrong.",
+            message: "Update failed. Please try again later.",
+          });
+      }
     });
-})
+  });
+});
 
-
-module.exports = { Register, AdminLogin, editAdmin};
+module.exports = { Register, AdminLogin, editAdmin };
